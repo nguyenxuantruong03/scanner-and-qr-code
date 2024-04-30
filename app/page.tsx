f6fd99b30ae7c3526184675708bd51332b8e55f1
asdiop159752 <nxt159753@gmail.com>
2024-04-30 17:37:57 +0700

    Camera and Scaner qr code

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,146 @@
-import Image from "next/image";
+// "use client";
+// import { Html5QrcodeScanner } from "html5-qrcode";
+// import Link from "next/link";
+// import { useEffect, useState } from "react";
 
-export default function Home() {
+// export default function Home() {
+//   const [scanResult, setScanResult] = useState<string | null>(null);
+//   useEffect(() => {
+//     const scanner = new Html5QrcodeScanner(
+//       "reader",
+//       {
+//         qrbox: {
+//           width: 250,
+//           height: 250,
+//         },
+//         fps: 5,
+//       },
+//       true // Add verbose as the third argument
+//     );
+
+//     scanner.render(success, error);
+
+//     function success(result: any) {
+//       scanner.clear();
+//       setScanResult(result);
+//     }
+
+//     function error(error: any) {
+//       console.warn(error);
+//     }
+//   }, []);
+
+//   return (
+//     <div className="max-w-4xl mx-auto">
+//       {scanResult ? (
+//         <div id="reader">
+//           <span className="text-center">Success: <Link className="underline text-sky-500" href={scanResult}>{scanResult}</Link></span>
+//         </div>
+//       ) : (
+//         <div id="reader" />
+//       )}
+//       <div id="reader" />
+//     </div>
+//   );
+// }
+
+"use client"
+import { useState, useRef, useEffect } from "react";
+
+const Camera = () => {
+  const videoRef = useRef(null);
+  const photoRef = useRef(null);
+  const [hasPhoto, setHasPhoto] = useState(false);
+  const [facingMode, setFacingMode] = useState("environment"); // Default to back camera
+
+  const getVideo = () => {
+    // Kiểm tra xem truy cập vào camera đã được phép hay không
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      // Yêu cầu truy cập vào camera với constraints mong muốn
+      navigator.mediaDevices
+        .getUserMedia({
+          video: {
+            width: 1280,
+            height: 720,
+            facingMode: facingMode // Chọn camera trước hoặc sau
+          },
+        })
+        .then((stream) => {
+          let video = videoRef.current as HTMLVideoElement | null; 
+          if (video) {
+            video.srcObject = stream;
+            video.play();
+          } else {
+            console.error("Video element not found.");
+          }
+        })
+        .catch((err) => {
+          console.error("Error accessing camera:", err);
+        });
+    } else {
+      console.error("getUserMedia is not supported by this browser");
+    }
+  };
+
+  const takePhoto = () => {
+    const width = 414;
+    const height = width / (16 / 9);
+
+    let video = videoRef.current;
+    let photo = photoRef.current as HTMLCanvasElement | null;
+
+    if (photo) {
+      photo.width = width;
+      photo.height = height;
+
+      let ctx = photo.getContext("2d");
+
+      if (ctx) {
+        if (video) {
+          ctx.drawImage(video, 0, 0, width, height);
+          setHasPhoto(true);
+        } else {
+          console.error("videoRef.current is null");
+        }
+      } else {
+        console.error("Failed to get 2D context for photo canvas");
+      }
+    } else {
+      console.error("photoRef.current is null");
+    }
+  };
+
+  const closePhoto = () =>{
+    let photo = photoRef.current as HTMLCanvasElement | null;
+   if(photo){
+    let ctx = photo.getContext('2d');
+    ctx?.clearRect(0, 0, photo.width, photo.height);
+   }
+    setHasPhoto(false)
+  }
+
+  const switchCamera = () => {
+    setFacingMode(facingMode === "user" ? "environment" : "user"); // Toggle between front and back camera
+    setHasPhoto(false); // Reset photo state when switching cameras
+  };
+
+  useEffect(() => {
+    getVideo();
+  }, [facingMode]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="container">
+      <div className="camera">
+        <video ref={videoRef}></video>
+        <button onClick={takePhoto}>SNAP</button>
+        <button onClick={switchCamera}>SWITCH CAMERA</button>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={"result " + (hasPhoto ? "hasPhoto" : "")}>
+        <canvas ref={photoRef}></canvas>
+        <button onClick={closePhoto}>CLOSE!</button>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default Camera;
